6c5cf14d79cc8a52d647668d2266046ba945f38c
andrewhjelle <andrewhjelle@gmail.com>
2017-04-29 01:17:51 -0500

    Add files via upload

---
+++ media.py
@@ -0,0 +1,16 @@
+import urllib
+import webbrowser
+
+class Movie():
+    '''Creates a shell for a tile to describe a movie with basic characteristics'''
+    # Creates the formatting for a movie tile
+    def __init__(self, movie_title, date_released, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.release_date = date_released
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    # Creates the ability to launch the movie trailer url
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
